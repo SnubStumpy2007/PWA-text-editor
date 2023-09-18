e0599de48031ada1212feb26d981ee44cb4d27d9
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-18 17:35:25 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PWA-text-editor
+
+https://codingartistweb.com/2022/04/rich-text-editor-with-javascript/
--- app.js
+++ app.js
@@ -114,4 +114,15 @@ const highlighterRemover = (className) => {
   });
 };
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
+
 window.onload = initializer();
